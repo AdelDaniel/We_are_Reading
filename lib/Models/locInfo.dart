f8624409786fab0d331996260bfb8f3e0ed65b07
Mahmoud Shawky <47181249+Ma7moudShawky@users.noreply.github.com>
2020-03-16 22:06:35 +0200

    Add files via upload

---
+++ lib/Models/locInfo.dart
@@ -0,0 +1,6 @@
+class locInfo{
+   double latitude;
+   double longitude;
+   String address;
+  locInfo({this.longitude,this.latitude,this.address});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import './screens/opening_screen.dart';
+import './screens/login_screen.dart';
+import './screens/signupLibrary_screen.dart';
+import './Screens/signupBorrower.dart';
+import './Providers/location_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'MyShop',
-      theme: ThemeData(
-        primaryColor: Color.fromRGBO(251, 192, 45, 1),
-        //accentColor: Colors.deepOrange,
-        fontFamily: 'Poppins',
-        textTheme: ThemeData.light().textTheme.copyWith(
-              body1: TextStyle(
-                color: Color.fromRGBO(63, 61, 86, 1),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: locationinfo()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'MyShop',
+        theme: ThemeData(
+          primaryColor: Color.fromRGBO(251, 192, 45, 1),
+          //accentColor: Colors.deepOrange,
+          fontFamily: 'Poppins',
+          textTheme: ThemeData.light().textTheme.copyWith(
+                body1: TextStyle(
+                  color: Color.fromRGBO(63, 61, 86, 1),
+                ),
               ),
-            ),
+        ),
+        home: Opening(),
+        routes: {
+          LoginScreen.routeName: (context) => LoginScreen(),
+          signupLib.routeName: (context) => signupLib(),
+          signupBorrower.routeName: (context) => signupBorrower(),
+        },
       ),
-      home: Opening(),
     );
   }
 }
