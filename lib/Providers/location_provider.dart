b758f8895d2bb78919e026d6ef34dadc213af766
Mahmoud Shawky <47181249+Ma7moudShawky@users.noreply.github.com>
2020-03-21 22:25:27 +0200

    Add files via upload

--- lib/Providers/location_provider.dart
+++ lib/Providers/location_provider.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:provider/provider.dart';
-import '../Models/locInfo.dart';
-class locationinfo with ChangeNotifier{
+import 'dart:io';
 
+class locationinfo with ChangeNotifier {
   Future<String> getPlaceAddress(double lat, double lng) async {
-    final url =
-        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=AIzaSyBc4tsmfaIs5LGku1_vUrhdOuLR5zJnX7k';
-    final response = await http.get(url);
-    notifyListeners();
-    return json.decode(response.body)['results'][0]['formatted_address'];
+    try {
+      final result = await InternetAddress.lookup('google.com');
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        final url =
+            'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=AIzaSyBc4tsmfaIs5LGku1_vUrhdOuLR5zJnX7k';
+        final response = await http.get(url);
+        notifyListeners();
+        return json.decode(response.body)['results'][0]['formatted_address'];
+      }
+    } on SocketException catch (_) {
+      print('Dis');
+      notifyListeners();
+      return null;
+    }
   }
-
 }
